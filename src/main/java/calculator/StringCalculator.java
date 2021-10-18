cbe6d20ce70d9f6001e992140e38363bd1e1554c
Aakash Patel <you@example.com>
2021-10-18 11:56:46 +0530

    The method can handle new lines between numbers and return sum

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -19,17 +19,43 @@ class StringCalculator {
     		return Integer.parseInt(arr[0]);
     	}
     	else  {
-    		return getSum(arr);
+    		return getSumTwo(arr);
     	}
     }
     
-    private int getSum(String[] numbers)  {
+    private int getSumTwo(String[] numbers)  {
     	int sum = 0;
     	for(int index = 0; index < numbers.length;index++) {
     		sum = Integer.parseInt(numbers[index]) + sum;
     	}
     	return sum;
     }
+    
+    public int addThree(String input) {
+    	
+    	String [] arr = input.split(",|\n");
+    	
+        if(input.isEmpty()) {
+        	return 0;
+        }else if(arr.length == 1) {
+    		return Integer.parseInt(arr[0]);
+    	}
+    	else  {
+    		return getSumThree(arr);
+    	}
+    }
+    
+    private int getSumThree(String[] numbers)  {
+    	int sum = 0;
+    	for(int index = 0; index < numbers.length;index++) {
+    		sum = Integer.parseInt(numbers[index]) + sum;
+    		
+    	}
+    	
+    	return sum;
+    }
+
+    
     
     
 
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -30,7 +30,11 @@ class StringCalculatorShould {
     	assertEquals(3, stringCalculator.add("1,1,1"));
     }
     
-   
+    @Test
+    void string_with_two_number_with_newLine_should_return_number_as_sum() throws Exception {
+    	StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(2, stringCalculator.addThree("1\n1"));
+    }
     
     
 
